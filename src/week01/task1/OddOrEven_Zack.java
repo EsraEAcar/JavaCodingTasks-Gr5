2e8141baa1be1a5f0c82051b31286873397f4782
ZackQA <zackqa01@gmail.com>
2024-01-29 23:17:09 +0100

    week 1 tasks solutions added

---
+++ src/week01/task1/OddOrEven_Zack.java
@@ -0,0 +1,15 @@
+package week01.task1;
+
+public class OddOrEven_Zack {
+
+    /*
+     Numbers - Odd or even:
+     Write  a method which can identify given number is even or odd
+     Output ex:
+     identify(5); ->"Odd"
+     identify(6); ->"Even"
+     */
+
+
+
+}
---
+++ src/week01/task2/DivideWithoutDivisionOperator_Zack.java
@@ -0,0 +1,12 @@
+package week01.task2;
+
+public class DivideWithoutDivisionOperator_Zack {
+
+     /*
+     Numbers - Divide without / operator:
+     Write a method that can divide two numbers without using division operator
+     */
+
+}
+
+
