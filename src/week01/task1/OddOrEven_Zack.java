228c3d225ca8bfc045e75045c6a9d8fd44132216
ZackQA <zackqa01@gmail.com>
2024-01-29 23:21:38 +0100

    week 1 tasks solutions update

--- src/week01/task1/OddOrEven_Zack.java
+++ src/week01/task1/OddOrEven_Zack.java
@@ -10,6 +10,18 @@ public class OddOrEven_Zack {
      identify(6); ->"Even"
      */
 
+    public static void main(String[] args) {
+        identifyOddEven(5); // ->  "Odd"
+        identifyOddEven(6); // ->  "Even"
+    }
 
+    /**
+     * returns whether the number given is Odd or Even
+     * @param number
+     * @return
+     */
+    public  static  String  identifyOddEven(int number) {
+        return  number%2==0 ? "Even" : "odd" ;
+    }
 
 }
--- src/week01/task3/FINRA_Zack.java
+++ src/week01/task3/FINRA_Zack.java
@@ -1,5 +1,7 @@
 package week01.task3;
 
+import java.util.Arrays;
+
 public class FINRA_Zack {
 
     /*
@@ -8,4 +10,63 @@ public class FINRA_Zack {
      For numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
      */
 
+    public static void main(String[] args) {
+        finra_1();
+        finra_2();
+        finra_3();
+    }
+
+    //******** SOLUTION 1 *********************
+    /**
+     * finra solution with multiple else-if block
+     */
+    public static void finra_1() {
+
+        String result = "";
+
+        for(int i=1; i <= 30; i++) {
+            if(i % 5==0 && i %3 ==0)
+                result += "FINRA ";
+            else if(i%5 == 0)
+                result += "RA ";
+            else if(i%3==0)
+                result+="FIN ";
+            else
+                result += i+" ";
+        }
+        System.out.println(result);
+    }
+
+    //******** SOLUTION 2 *********************
+    /**
+     * finra solution with ternary
+     */
+    public static void finra_2() {
+
+        String result = "";
+
+        for(int i=1; i <= 30; i++) {
+            result += (i % 5 ==0 && i %3 ==0)? "FINRA " : (i%5 == 0) ? "RA "
+                    :(i%3 == 0) ? "FIN " : i+" ";
+        }
+        System.out.println(result);
+    }
+
+    //******** SOLUTION 3 *********************
+    public static void finra_3() {
+        String[] myarr= new String[30];
+
+        for( int i=0; i <= 29; i++ )
+            myarr[i] = ""+(i+1);
+
+        for(int j=0; j<myarr.length; j++)
+            if(Integer.valueOf(myarr[j])%3==0 && Integer.valueOf(myarr[j])%5==0)
+                myarr[j]="FINRA";
+            else if (Integer.valueOf(myarr[j])%3==0)
+                myarr[j]="FIN";
+            else if (Integer.valueOf(myarr[j])%5==0)
+                myarr[j]="RA";
+
+        System.out.println(Arrays.toString(myarr));
+    }
 }
