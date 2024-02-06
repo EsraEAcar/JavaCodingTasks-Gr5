e0d315938141eb35e374e3fecea76e6f46fa86ef
erdemce <erdemce2234@gmail.com>
2024-02-06 21:55:52 +0100

    Task1 created

---
+++ src/week01/task1/OddOrEven_Sabriye.java
@@ -0,0 +1,21 @@
+package week01.task1;
+
+public class OddOrEven_Sabriye {
+    public static void main(String[] args) {
+
+
+            IsEvenOrOdd(98);
+
+        }
+
+
+        public static void IsEvenOrOdd(int num) {
+            if (num % 2 == 0) {
+                System.out.println("this number is even");
+            } else {
+                System.out.println("this number is odd");
+            }
+
+        }
+    }
+
