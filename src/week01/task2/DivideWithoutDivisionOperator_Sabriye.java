dbe88c20d55b3872a9f4a7559894430efedd5651
erdemce <erdemce2234@gmail.com>
2024-02-06 22:07:40 +0100

    Task2 created

---
+++ src/week01/task2/DivideWithoutDivisionOperator_Sabriye.java
@@ -0,0 +1,12 @@
+package week01.task2;
+
+public class DivideWithoutDivisionOperator_Sabriye {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+
+     Numbers - Divide without / operator:
+     Write a method that can divide two numbers without using division operator
+     */
