7406c9d51f5b1718234b1bd2d102a09c3d2cbed3
sabriyetaskin <sabriyem2234@gmail.com>
2024-02-07 12:29:47 +0100

    Task1, task2 updated

--- src/week01/task1/OddOrEven_Sabriye.java
+++ src/week01/task1/OddOrEven_Sabriye.java
@@ -2,20 +2,23 @@ package week01.task1;
 
 public class OddOrEven_Sabriye {
     public static void main(String[] args) {
+        IsEvenOrOdd(5);
+        IsEvenOrOdd(6);
+    }
 
-
-            IsEvenOrOdd(98);
-
-        }
-
-
-        public static void IsEvenOrOdd(int num) {
-            if (num % 2 == 0) {
-                System.out.println("this number is even");
-            } else {
-                System.out.println("this number is odd");
-            }
-
+    public static void IsEvenOrOdd(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
         }
     }
+}
+/*
+     Numbers - Odd or even:
+     Write  a method which can identify given number is even or odd
+     Output ex:
+     identify(5); ->"Odd"
+     identify(6); ->"Even"
+     */
 
--- src/week01/task2/DivideWithoutDivisionOperator_Sabriye.java
+++ src/week01/task2/DivideWithoutDivisionOperator_Sabriye.java
@@ -1,12 +1,35 @@
 package week01.task2;
 
 public class DivideWithoutDivisionOperator_Sabriye {
+
     public static void main(String[] args) {
-
+        System.out.println((divide(34,-3)));
+        System.out.println(34/-3);
     }
-}
-/*
 
+        public static int divide(int dividend, int divisor) {
+            if (divisor == 0) {
+                throw new ArithmeticException("Divisor cannot be zero");
+            }
+
+            int quotient = 0;
+            int sign = dividend * divisor < 0 ? -1 : 1;
+
+            dividend = dividend >= 0 ? dividend : (-1 * dividend);
+            divisor = divisor >= 0 ? divisor : (-1 * divisor);
+
+            while (dividend >= divisor) {
+                dividend -= divisor;
+                quotient++;
+            }
+
+            return sign * quotient;
+        }
+    }
+
+
+
+/*
      Numbers - Divide without / operator:
      Write a method that can divide two numbers without using division operator
      */
