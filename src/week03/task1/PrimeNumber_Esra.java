5df583d9c100dab387da4a6e8d28d90af092002e
EsraEAcar <ersenacar@yahoo.com>
2024-02-15 10:04:11 +0100

    updated code

---
+++ src/week03/task1/PrimeNumber_Esra.java
@@ -0,0 +1,39 @@
+package week03.task1;
+
+public class PrimeNumber_Esra {
+    public static void main(String[] args) {
+
+        isPrimeNumber(8);
+        isPrimeNumber(7);
+        isPrimeNumber(15);
+        isPrimeNumber(64);
+        isPrimeNumber(5);
+
+
+    }
+
+    public static void isPrimeNumber(int n) {
+
+String result = n + " is a prime number";
+
+        for (int i = 2; i < n; i++) {
+
+            if (n % i == 0) {
+                result = (n + " is not a prime number");
+            }
+        }
+
+        System.out.println(result);
+    }
+}
+
+
+
+
+
+
+
+/*
+1️⃣ Numbers -- PrimeNumber
+Write a method that can check if a number is prime or not
+ */
---
+++ src/week03/task2/ReverseNegativeNumber_Esra.java
@@ -0,0 +1,34 @@
+package week03.task2;
+
+public class ReverseNegativeNumber_Esra {
+    public static void main(String[] args) {
+
+
+
+
+            int givenNumber = -35;
+            String reversedNumber = "-";
+
+            String convertedIntToString = String.valueOf(givenNumber);
+
+            for(int i = convertedIntToString.length()-1; i >= 1; i--){
+                reversedNumber += convertedIntToString.charAt(i);
+
+            }
+
+        System.out.println(reversedNumber);
+        }
+    }
+
+
+
+
+
+
+
+
+/*
+2️⃣ Numbers -- Reverse Negative Number
+Write a return method that can reverse digits of a negative number and return it as int
+input: -35  output: -53
+ */
