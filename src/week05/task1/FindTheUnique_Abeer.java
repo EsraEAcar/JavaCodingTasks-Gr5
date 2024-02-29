52d23f6bc37929ec0de418795f24105f2e535e83
abeer <abeerabuomae22@gmail.com>
2024-02-29 15:31:35 +0100

    Abeer

---
+++ src/week05/task1/FindTheUnique_Abeer.java
@@ -0,0 +1,30 @@
+package week05.task1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FindTheUnique_Abeer {
+
+    public static void main(String[] args) {
+
+        System.out.println(UniqueChar("AAABBBCCCDEF"));
+
+
+    }
+
+    public static String UniqueChar(String str)
+    {
+
+        String unique="";
+        String [] array=str.split("");
+        for (String each : array ) {
+            int Unique = Collections.frequency( Arrays.asList( array )  , each );
+            if(Unique == 1)
+             unique+=each   ;
+        }
+
+        return unique;
+    }
+
+
+}
