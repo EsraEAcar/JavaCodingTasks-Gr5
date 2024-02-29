9f9fd5d7b863010302ef0dd5bddc0ed7e872a0d4
abeer <abeerabuomae22@gmail.com>
2024-02-29 16:10:35 +0100

    Abeer

--- src/week05/task1/FindTheUnique_Abeer.java
+++ src/week05/task1/FindTheUnique_Abeer.java
@@ -23,6 +23,7 @@ public class FindTheUnique_Abeer {
              unique+=each   ;
         }
 
+
         return unique;
     }
 
--- src/week05/task2/Reverse_Abeer.java
+++ src/week05/task2/Reverse_Abeer.java
@@ -6,6 +6,7 @@ public class Reverse_Abeer {
 
     public static void main(String[] args) {
 
+
         System.out.println(Reverse("ABCD"));
 
     }
