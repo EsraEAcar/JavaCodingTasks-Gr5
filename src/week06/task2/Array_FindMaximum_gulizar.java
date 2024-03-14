7fe1831c3accc2e58a838ce9367e735191590925
gulizaer <146580308+gulizaer@users.noreply.github.com>
2024-03-14 14:30:09 +0100

    week06 solution

---
+++ src/week06/task2/Array_FindMaximum_gulizar.java
@@ -0,0 +1,23 @@
+package week06.task2;
+
+public class Array_FindMaximum_gulizar {
+    public static void main(String[] args) {
+        int[] arr = {99, 12, 23, 32, 44, 57, 6};
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (max > arr[i]){
+
+            }
+
+        }
+        System.out.println(max);
+
+    }
+}
+/*
+ Array_FindMaximum
+Write a method that can find the maximum number from an int Array
+Ex:  int[] arr = {99, 12, 23, 32, 44, 57, 6} ==> 99
+
+ */
